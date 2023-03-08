9fc5469bcc715064fa85646b3ddc20f4443ce5f8
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-03-08 14:07:20 +0530

    Added get request

--- src/components/Layout/Entries.tsx
+++ src/components/Layout/Entries.tsx
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Entry from "./Entry";
 
+const baseURL = process.env.REACT_APP_BASE_URL;
+
+type SelectProps = {
+  title: string;
+  companyName: string;
+  industry: string;
+  location: string;
+  remoteType: string;
+  expMin: number;
+  expMax: number;
+  totalEmp: number;
+  applyType: boolean;
+  salaryMin: number;
+  salaryMax: number;
+  quick: boolean;
+  id: string;
+};
+
 function Entries() {
-  var data = [
+  let data: SelectProps[] = [
     {
       title: "UX UI Designer",
       companyName: "Great View",
@@ -33,11 +53,26 @@ function Entries() {
       id: "1",
     },
   ];
+
+  const [jobs, setJobs] = useState<SelectProps[] | null | void>([]);
+  console.log("base", baseURL);
+  console.log("base1", process.env.REACT_APP_BASE_URL);
+
+  useEffect(() => {
+    axios
+      .get<SelectProps[]>(`${baseURL}/tasks`)
+      .then((response) => {
+        console.log(response.data);
+        setJobs(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className=" grid md:grid-cols-[50%_50%] grid-cols-[100%] grid-rows-[auto] gap-x-5 gap-y-2.5 bg-[#D8D8D8] md:py-5 md:px-12 py-2 px-5">
-      {data.map((job) => {
-        return <Entry job={job} />;
-      })}
+      {jobs != null && jobs.map((job: SelectProps) => <Entry job={job} />)}
     </div>
   );
 }
